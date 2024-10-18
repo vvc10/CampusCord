5fb83b38ac486dcf465703ad7bc78013c03aed1c
Pankaj Yadav <vvastcore@gmail.com>
2024-10-19 00:29:25 +0530

    :

--- client-app/src/serviceWorkerRegistration.js
+++ client-app/src/serviceWorkerRegistration.js
@@ -20,7 +20,7 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // Define the service worker URL directly
-    const swUrl = `${window.location.origin}/service-worker.js`;
+    const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
     if (isLocalhost) {
       // This is running on localhost. Let's check if a service worker still exists or not.
